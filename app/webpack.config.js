a10b27f6cc55a215321a93c5376c715d4a0d567e
Ahmed Khattab <ahmed@aktrading.io>
2022-12-09 14:10:10 +0200

    first commit

---
+++ app/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CopyWebpackPlugin({ patterns: [{ from: "./src/index.html", to: "index.html" }] }),
+  ],
+  devServer: { static: path.resolve(__dirname, 'dist'), compress: true },
+};
